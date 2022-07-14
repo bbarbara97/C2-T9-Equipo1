e6cada68009f8444e952432aa6618bb496a01223
oandreazini <oriol_as_92@hotmail.com>
2022-07-14 11:04:56 +0200

    Merge remote-tracking branch 'origin/television' into electrodomestico

--- RTS-C2-UD09/src/ex1/Lavadora.java
+++ RTS-C2-UD09/src/ex1/Lavadora.java
@@ -1,5 +1,14 @@
 package ex1;
 
 public class Lavadora {
+	
+	private double carga;
+	
+	private final double CARGA_D = 5.0;
+	
+	//Constructores
+		public Lavadora() {
+			this.carga = CARGA_D;
+		}
 
 }
