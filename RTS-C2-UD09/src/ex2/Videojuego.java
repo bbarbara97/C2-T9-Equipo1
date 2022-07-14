3e5d515fd8dafeff70e084d2a0728d5aeef2a6d3
oandreazini <oriol_as_92@hotmail.com>
2022-07-14 17:24:20 +0200

    Videojuego

---
+++ RTS-C2-UD09/src/ex2/Videojuego.java
@@ -0,0 +1,77 @@
+package ex2;
+
+public class Videojuego implements Entregable {
+	
+	protected String titulo;
+	protected int nHoras;
+	protected boolean entregado;
+	protected String genero;
+	protected String compañia;
+	
+	//Constantes
+		protected final int N_HORAS = 10;
+		protected final boolean ENTREGADO_D = false;
+		
+		//Constructores
+		public Videojuego() {
+			this.titulo = "";
+			this.nHoras = N_HORAS;
+			this.entregado = ENTREGADO_D;
+			this.genero = "";
+			this.compañia = "";
+		}
+		
+		public Videojuego(String titulo, int nHoras) {
+			this.titulo = titulo;
+			this.nHoras = nHoras;
+			this.entregado = ENTREGADO_D;
+			this.genero = "";
+			this.compañia = "";
+		}
+		
+		public Videojuego(String titulo, int nHoras, String genereo, String compañia) {
+			this.titulo = "";
+			this.nHoras = nHoras;
+			this.entregado = ENTREGADO_D;
+			this.genero = genereo;
+			this.compañia = compañia;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public int getnHoras() {
+			return nHoras;
+		}
+
+		public void setnHoras(int nHoras) {
+			this.nHoras = nHoras;
+		}
+
+		public String getGenero() {
+			return genero;
+		}
+
+		public void setGenero(String genero) {
+			this.genero = genero;
+		}
+
+		public String getCompañia() {
+			return compañia;
+		}
+
+		public void setCompañia(String compañia) {
+			this.compañia = compañia;
+		}
+
+		@Override
+		public String toString() {
+			return "Videojuego [titulo=" + titulo + ", nHoras=" + nHoras + ", entregado=" + entregado + ", genero="
+					+ genero + ", compañia=" + compañia + "]";
+		}
+}
