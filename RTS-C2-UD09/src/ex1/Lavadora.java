fff99112f7350bb50f676c58ea018519f6cca3e4
pbesora <pau_besora@hotmail.com>
2022-07-15 10:19:18 +0200

    Fix precioFinal()

--- RTS-C2-UD09/src/ex1/Lavadora.java
+++ RTS-C2-UD09/src/ex1/Lavadora.java
@@ -24,37 +24,8 @@ public class Lavadora extends Electrodomestico{
 
 	//Metodos
 	public void precioFinal() {
-		switch (this.consumo) {
-		case 'A':
-			this.precioFinal += 100;
-			break;
-		case 'B':
-			this.precioFinal += 80;
-			break;
-		case 'C':
-			this.precioFinal += 60;
-			break;
-		case 'D':
-			this.precioFinal += 50;
-			break;
-		case 'E':
-			this.precioFinal += 30;
-			break;
-		case 'F':
-			this.precioFinal += 10;
-			break;
-		default:
-			break;
-		}
-		if(this.peso <=19) {
-			this.precioFinal += 10;
-		} else if(this.peso <=49) {
-			this.precioFinal += 50;
-		}else if(this.peso <=79) {
-			this.precioFinal += 80;
-		} else {
-			this.precioFinal += 100;
-		}
+		
+		super.precioFinal();
 		
 		if(this.carga > 30) {
 			precioFinal += 50;
