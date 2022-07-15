d9594c1a4f3cd9b0a880e863f27a7c8108c0b385
Bárbara Broto <barbara.broto97@gmail.com>
2022-07-15 08:22:29 +0200

    Creado ej 4 con main y Raices

---
+++ RTS-C2-UD09/src/ex4/Raices.java
@@ -0,0 +1,46 @@
+package ex4;
+
+public class Raices {
+
+	private int a;
+	private int b;
+	private int c;	
+
+	//Constructor
+	public Raices(int a, int b, int c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	//Métodos
+	public double getDiscriminante(){
+
+		return 0.0;
+	}
+
+	public boolean tieneRaices(){
+
+		return true;
+	}
+
+	public boolean tieneRaiz(){
+
+		return true;
+	}
+
+	public void calcular(){
+
+	}
+
+	public void obtenerRaices(){
+
+	}
+
+	public void obtenerRaiz(){
+
+	}
+
+
+}
