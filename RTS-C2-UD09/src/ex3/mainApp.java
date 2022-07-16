13ddac1ca5a24d01f6ff6675cd737e41672c18e8
Bárbara Broto <barbara.broto97@gmail.com>
2022-07-16 17:59:32 +0200

    Optimizado ej 3

--- RTS-C2-UD09/src/ex3/mainApp.java
+++ RTS-C2-UD09/src/ex3/mainApp.java
@@ -3,16 +3,21 @@ package ex3;
 public class mainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Libro[] Libros = new Libro[2];
-
+		rellenarArray(Libros);
+		masPaginas(Libros);
+	}
+	
+	public static void rellenarArray(Libro[] Libros) {
 		Libros[0] = new Libro("1234567891012", "Fundación Esplai", "Bárbara", 200);
 		Libros[1] = new Libro("9876543210123", "T-Systems", "Pau", 100);
-
+		
 		System.out.println(Libros[0].toString());
 		System.out.println(Libros[1].toString());
-		
+	}
+	
+	public static void masPaginas(Libro[] Libros) {
 		if(Libros[0].getnPaginas()>Libros[1].getnPaginas()) {
 			System.out.println("El libro con más paginas es: " + Libros[0].toString());
 		} else if(Libros[0].getnPaginas()<Libros[1].getnPaginas()){
