d229dc7be9d4199f52325e82547cdcfcd43caa69
oandreazini <oriol_as_92@hotmail.com>
2022-07-17 11:35:06 +0200

    Comentarios ex1

--- RTS-C2-UD09/src/ex1/Television.java
+++ RTS-C2-UD09/src/ex1/Television.java
@@ -1,37 +1,63 @@
 package ex1;
 
+/**
+ * Clase Television
+ * Contiene informacion de television
+ * @author Barbra, Pau, Oriol
+ */
 public class Television extends Electrodomestico{
 
-	//Constantes
+	/*CONSTANTES*/
 	private final double RESOLUCION_D = 20.0;
 	private final boolean TDT_D = false;
 	
+	/*ATRIBUTOS*/
 	private Double resolucion = RESOLUCION_D;
 	private boolean tdt = TDT_D;
 	
 	
-	//Constructores
+	/**CONSTRUCTORES**/
+	
+	//Constructor por defecto
 	public Television() {
 		this.resolucion = RESOLUCION_D;
 		this.tdt = TDT_D;
 	}
 	
-	
+	/**
+	 * Constructor con 2 parametros
+	 * @param precioBase
+	 * @param peso
+	 */
 	public Television(Double precioBase, Double peso) {
 		super(precioBase,peso);
 		this.resolucion = RESOLUCION_D;
 		this.tdt = TDT_D;
 	}
 	
+	/**
+	 * Constructor con 6 parametros
+	 * @param precioBase
+	 * @param peso
+	 * @param color
+	 * @param consumo
+	 * @param resolucion
+	 * @param tdt
+	 */
 	public Television(Double precioBase, Double peso, String color, char consumo, Double resolucion, boolean tdt) {
 		super(precioBase, peso, color, consumo);
 		this.resolucion = resolucion;
 		this.tdt = tdt;
 	}
 	
-	//Metodos
+	/**	METODOS **/
+	
+	/**
+	 * Calcula el precio final respecto a la resolucion
+	 * si es superior a 40 le suma 30% respecto al precio base, 
+	 * i si tiene tdt le suma 50 a precio final 
+	 */
 	public void precioFinal() {
-		
 		super.precioFinal();
 		
 		if(this.resolucion > 40) {
@@ -42,7 +68,8 @@ public class Television extends Electrodomestico{
 		}
 	}
 	
-	//Getters
+	/**GETTERS**/
+
 	public double getResolucion() {
 		return resolucion;
 	}
--- RTS-C2-UD09/src/ex1/mainApp.java
+++ RTS-C2-UD09/src/ex1/mainApp.java
@@ -1,15 +1,23 @@
 package ex1;
 
+/**
+ * Clase ejecutable
+ * @author Barbra, Pau, Oriol
+ */
 public class mainApp {
 
 	public static void main(String[] args) {
-
+		/**
+		 *Creamos un array de electrodomestcos y llamamos a los metodos
+		 *para registrarlos y separarlos por utilidades
+		 */
 		Electrodomestico[] electrodomesticos = new Electrodomestico[10];
 		registrarElectrodomestico(electrodomesticos);
 		utilidades(electrodomesticos);
 	}
-	
+
 	public static void registrarElectrodomestico(Electrodomestico[] electrodomesticos) {
+		//Asignamos las posiciones del array
 		electrodomesticos[0] = new Lavadora(150.0, 200.0, "blanco", 'C',40.0);
 		electrodomesticos[1] = new Lavadora(150.0, 10.0, "rojo", 'B',60.0);
 		electrodomesticos[2] = new Lavadora(150.0, 25.0, "azul", 'F',30.0);
@@ -22,14 +30,18 @@ public class mainApp {
 		electrodomesticos[8] = new Television(150.0, 45.0, "blanco", 'J',20.0, false);
 		electrodomesticos[9] = new Television(150.0, 150.0, "negro", 'E',40.0, true);
 	}
-	
-	public static void utilidades(Electrodomestico[] electrodomesticos) {
-		for(int i = 0; i < 10; i++) {
-			electrodomesticos[i].precioFinal();
-		}
 
+	public static void utilidades(Electrodomestico[] electrodomesticos) {
+		
 		double precioTvs=0, precioLavadoras = 0;
-		for(int i =0; i<10; i++) {
+		/**
+		*Recorremos el array de electrodomesticos, si encuentra una tv o una lavadora
+		*pasara por su clase y por la de electrodomesticos, y se ejecutara en cada uno 
+		*sus propios metodos para el calculo de precio final
+		*/
+		for(int i = 0; i < electrodomesticos.length; i++) {
+
+			electrodomesticos[i].precioFinal();
 
 			if(electrodomesticos[i] instanceof Television) {
 				precioTvs += electrodomesticos[i].getPrecioFinal();
@@ -38,13 +50,14 @@ public class mainApp {
 				precioLavadoras += electrodomesticos[i].getPrecioFinal();
 			}
 		}
-		
+
 		System.out.println("El precio total de las lavadoras es: " + precioLavadoras + " €");
 		System.out.println("El precio total de las televisiones es: " + precioTvs + " €");
-		
+
 		double precioSuma =0;
 		
-		for(int i=0; i<10; i++) {
+		//Recorre el array electrodomesticos y suma todos los precios de todas las posiciones
+		for(int i=0; i < electrodomesticos.length; i++) {
 			precioSuma += electrodomesticos[i].getPrecioFinal();
 		}
 		System.out.print("El precio total de los electrodomesticos es: " + precioSuma + " €");
