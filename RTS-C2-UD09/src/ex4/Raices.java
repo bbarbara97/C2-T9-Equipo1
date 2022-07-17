ec206bb5fd0df93784ac5245375cbb7f7bfcf658
oandreazini <oriol_as_92@hotmail.com>
2022-07-17 12:50:22 +0200

    Comentarios ex4

--- RTS-C2-UD09/src/ex4/Raices.java
+++ RTS-C2-UD09/src/ex4/Raices.java
@@ -1,33 +1,47 @@
 package ex4;
 
+/**
+ * Clase Raices
+ * Calcula una ecuacion de 2º grado
+ * @author Bárbara, Pau, Oriol
+ */
 public class Raices {
 
+	/**ATRIBUTOS**/
 	private int a;
 	private int b;
 	private int c;	
 
-	//Constructor
+	//**CONSTRUCTORES**/
+
+	/**
+	 * Valores de la ecuacion
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
 	public Raices(int a, int b, int c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
 
-	//Métodos
-	public double getDiscriminante(){
+	/**	METODOS **/
 
+	//Nos devuelve el valor del discriminante
+	public double getDiscriminante(){
 		return Math.pow(b, 2) - 4*a*c;
 	}
 
+	//Si el discriminante es mayor a 0 nos devuelve true, sino false
 	public boolean tieneRaices(){
-
 		if(getDiscriminante()>0)
 			return true;
 		else
 			return false;
-
 	}
 
+	//Si el discriminante es igual a 0 nos devuelve true, sino false
 	public boolean tieneRaiz(){
 		if(getDiscriminante()==0)
 			return true;
@@ -35,10 +49,9 @@ public class Raices {
 			return false;
 	}
 
+	//Calcula la ecuacion de 2º grado
 	public void calcular(){
-
 		if(tieneRaices() == true) {
-
 			double resultado =	(-b-Math.sqrt(getDiscriminante()))/(2*a);
 			double resultado1 = (-b+Math.sqrt(getDiscriminante()))/(2*a);
 
@@ -50,8 +63,8 @@ public class Raices {
 		}
 	}
 
+	//Obtiene las raices cuando hay dos soluciones
 	public void obtenerRaices(){
-
 		if(tieneRaices() == true) {
 			double resultado= -Math.sqrt(getDiscriminante());
 			double resultado1 = +Math.sqrt(getDiscriminante());
@@ -60,14 +73,12 @@ public class Raices {
 		}
 	}
 
+	//Obtiene la raiz cuando hay 1 solucion
 	public void obtenerRaiz(){
 		if(tieneRaiz() == true) {
 
 			double resultado1 = +Math.sqrt(getDiscriminante());
 			System.out.println("La raiz es: " +resultado1);
 		}
-
 	}
-
-
 }
